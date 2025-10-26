2883bee36c24ba78c785ca4493ba0df70980b54f
alexisgtierrz <alexisgutierrez1072@gmail.com>
2025-10-26 18:51:39 -0300

    Implementacion para encontrar empleados por DNI, Telefono y Email

--- src/main/java/com/rrhh/Recursos_Humanos/Repositorios/EmpleadoRepository.java
+++ src/main/java/com/rrhh/Recursos_Humanos/Repositorios/EmpleadoRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
     List<Empleado> findByPuestoId(Long puestoId);
+    Optional<Empleado> findByDni(String dni);
+    Optional<Empleado> findByEmail(String email);
+    Optional<Empleado> findByTelefono(String telefono);
     boolean existsByDni(String dni);
     boolean existsByEmail(String email);
     boolean existsByTelefono(String telefono);
